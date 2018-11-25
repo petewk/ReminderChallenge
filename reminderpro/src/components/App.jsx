0fd6199976767419a30ff9a2d90f6dcb57a36869
petewk <petewk@users.noreply.github.com>
2018-11-25 13:39:15 +0000

    Redux
    Now logs state to store

--- reminderpro/src/components/App.jsx
+++ reminderpro/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addReminder } from '../actions';
-import { bindActionCreators } from 'redux';
+
 
 
 class App extends Component {
@@ -14,7 +14,23 @@ class App extends Component {
 
   addReminder() {
     this.props.addReminder(this.state.text)
+  }
 
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+      {
+        reminders.map(reminder => {
+          return (
+            <li key={reminder.id} className="list-group-item">
+              <div>{reminder.text}</div>
+            </li>
+          )
+        })
+      }
+      </ul>
+    )
   }
 
   render() {
@@ -31,6 +47,7 @@ class App extends Component {
             onChange={event => this.setState({text: event.target.value})}
             />
           </div>
+          { this.renderReminders() }
           <button type="button"
           className="btn btn-success"
           onClick={() => this.addReminder()}>
@@ -43,8 +60,10 @@ class App extends Component {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({addReminder}, dispatch);
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, { addReminder })(App);
